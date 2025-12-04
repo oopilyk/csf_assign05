650f97e7a6230ada5c12d6edef73186db8bd045e
Kyle Li <kli95@gmail.com>
2025-12-04 17:44:54 -0500

    fixed

--- message_queue.cpp
+++ message_queue.cpp
@@ -1,18 +1,16 @@
 #include <cassert>
 #include <ctime>
+#include "message.h"
 #include "message_queue.h"
 
 MessageQueue::MessageQueue() {
   //initializes the mutex and the semaphore
-
   pthread_mutex_init(&m_lock, nullptr);
   sem_init(&m_avail, 0, 0);
-
 }
 
 MessageQueue::~MessageQueue() {
   //destroys the mutex and the semaphore
-
   pthread_mutex_lock(&m_lock);
 
   while(!m_messages.empty()) {
@@ -25,12 +23,10 @@ MessageQueue::~MessageQueue() {
 
   pthread_mutex_destroy(&m_lock);
   sem_destroy(&m_avail);
-
 }
 
 void MessageQueue::enqueue(Message *msg) {
   //puts the specified message on the queue
-
   pthread_mutex_lock(&m_lock);
 
   m_messages.push_back(msg);
@@ -38,26 +34,11 @@ void MessageQueue::enqueue(Message *msg) {
   pthread_mutex_unlock(&m_lock);
 
   sem_post(&m_avail);
-
 }
 
 Message *MessageQueue::dequeue() {
-  struct timespec ts;
-
-  // gets the current time using clock_gettime:
-  // we don't check the return value because the only reason
-  // this call would fail is if we specify a clock that doesn't
-  // exist
-  clock_gettime(CLOCK_REALTIME, &ts);
-
-  // computes a time one second in the future
-  ts.tv_sec += 1;
-
-  Message *msg = nullptr;
-
-  if (sem_timedwait(&m_avail, &ts) == -1) {
-      // No message available within 1 second
-      return msg;
+  if (sem_wait(&m_avail) == -1) {
+      return nullptr;
   }
 
   pthread_mutex_lock(&m_lock);
@@ -68,5 +49,4 @@ Message *MessageQueue::dequeue() {
   pthread_mutex_unlock(&m_lock);
 
   return msg;
-  
 }
